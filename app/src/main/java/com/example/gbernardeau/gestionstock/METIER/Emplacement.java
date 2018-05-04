355dafc9693f1a1b1684f42699033c42e9bdb2d8
MarcouxDesBois <marco.rds22@gmail.com>
2018-05-04 11:42:19 +0200

    Ajout commentaire Emplacement.

--- app/src/main/java/com/example/gbernardeau/gestionstock/METIER/Emplacement.java
+++ app/src/main/java/com/example/gbernardeau/gestionstock/METIER/Emplacement.java
@@ -1,5 +1,13 @@
 package com.example.gbernardeau.gestionstock.METIER;
 
+/*
+
+* Nom de la classe : Emplacement
+* Ajoutée le 04/05/2018
+* Descriptif : Classe permettant de gérer un objet Emplacement.
+
+*/
+
 public class Emplacement {
 
     private int id;
@@ -9,6 +17,14 @@ public class Emplacement {
         this.id = id;
         this.libelle = libelle;
     }
+
+    /*
+
+     * Ajoutée le 04/05/2018
+     * Descriptif : Méthodes permettant d'obtenir l'Id et le libellé d'un emplacement ainsi que ses
+     * setter pour l'Id et le libellé.
+
+     */
 
     public int getId() {
         return id;
