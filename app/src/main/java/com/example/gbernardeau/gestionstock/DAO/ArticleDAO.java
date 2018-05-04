8a08ab7733c52eefa8c2482323933a82c0cf884d
Gael <gaelbernardeau@gmail.com>
2018-05-04 09:27:57 +0200

    Ajout article DAO

---
+++ app/src/main/java/com/example/gbernardeau/gestionstock/DAO/ArticleDAO.java
@@ -0,0 +1,102 @@
+package com.example.gbernardeau.gestionstock.DAO;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import com.example.gbernardeau.gestionstock.METIER.Article;
+
+import java.util.ArrayList;
+
+
+public class ArticleDAO extends DAO<Article> {
+
+    private SQLiteGestionStock dbGestionStock;
+
+    private static final String Table_ARTICLE = "ARTICLE";
+    private static final String COL_ID_ARTICLE = "ID";
+    private static final String COL_DESIGNATION_ARTICLE = "DESIGNATION";
+    private static final String COL_STOCK_ARTICLE = "STOCK";
+
+    private SQLiteDatabase db;
+
+    public ArticleDAO(Context context) {
+        SQLiteOpenHelper dbGestionStock = new SQLiteGestionStock(context);
+    }
+
+    public void open() {
+        db = dbGestionStock.getWritableDatabase();
+    }
+
+    public void close() {
+        db.close();
+    }
+
+    public boolean insert(Article ma) {
+        boolean res;
+        long insert;
+        res = false;
+        ContentValues mavaleur = new ContentValues();
+        mavaleur.put(COL_DESIGNATION_ARTICLE, ma.getDesignation());
+        mavaleur.put(COL_STOCK_ARTICLE, ma.getStock());
+        insert = db.insert(Table_ARTICLE, null, mavaleur);
+        if (insert != -1) {
+            res = true;
+        }
+        return res;
+    }
+
+    public boolean update(Article obj) {
+        boolean res;
+        int update;
+        res = false;
+        ContentValues mavaleur = new ContentValues();
+        mavaleur.put(COL_DESIGNATION_ARTICLE, obj.getDesignation());
+        mavaleur.put(COL_STOCK_ARTICLE, obj.getStock());
+        update = db.update(Table_ARTICLE, mavaleur, null, null);
+        if (update > 0) {
+            res = true;
+        }
+        return res;
+    }
+
+    public boolean delete(Article obj) {
+        boolean res;
+        int delete;
+        res = false;
+        delete = db.delete(Table_ARTICLE, null, null);
+        if (delete > 0) {
+            res = true;
+        }
+        return res;
+    }
+
+    public Article read(long id) {
+        Cursor res = db.query(Table_ARTICLE, null, null, null, null, null, null);
+        Article unArticle = new Article(res.getString(0), res.getString(1), res.getInt(2));
+        return unArticle;
+    }
+
+    public ArrayList<Article> read() {
+        ArrayList<Article> listArticle = new ArrayList<Article>();
+        int id;
+        String lib;
+        float coeff;
+        Article ma;
+        Cursor res;
+        res = db.query(Table_ARTICLE, null, null, null, null, null, null);
+        res.moveToFirst();
+        while (!res.isAfterLast()) {
+            id = res.getInt(0);
+            lib = res.getString(1);
+            coeff = res.getFloat(2);
+            ma = new Article("code", "lib", 0);
+            listArticle.add(ma);
+        }
+
+        return listArticle;
+    }
+
+}
--- app/src/main/java/com/example/gbernardeau/gestionstock/DAO/SQLiteGestionStock.java
+++ app/src/main/java/com/example/gbernardeau/gestionstock/DAO/SQLiteGestionStock.java
@@ -5,12 +5,12 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 
-public class SQLiteGestionNotes extends SQLiteOpenHelper{
+public class SQLiteGestionStock extends SQLiteOpenHelper{
 
     private static final int DATABASE_VERSION = 1;
     private static final String DATABASE_NAME = "Gestiondesnotes";
     private Context context = null;
-    public SQLiteGestionNotes(Context context){
+    public SQLiteGestionStock(Context context){
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
         this.context = context;
     }
