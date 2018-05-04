c1e2534aa9ee2f63ca5a77ca193126aee96ce593
Gael <gaelbernardeau@gmail.com>
2018-05-04 09:14:46 +0200

    Ajout de classe metier

---
+++ app/src/main/java/com/example/gbernardeau/gestionstock/METIER/Famille_Article.java
@@ -0,0 +1,10 @@
+package com.example.gbernardeau.gestionstock.METIER;
+
+/**
+ * Created by gbernardeau on 04/05/2018.
+ */
+
+public class Famille_Article {
+    private int idfamille;
+    private String code;
+}
